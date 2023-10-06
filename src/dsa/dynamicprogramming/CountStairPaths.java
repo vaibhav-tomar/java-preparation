707f37fe654927d636c3f0e376c98d0e785e1758
vaibhav-tomar <vaibhavtomar.tmr@gmail.com>
2023-10-06 16:40:32 +0530

    Add more questions

---
+++ src/dsa/dynamicprogramming/CountStairPaths.java
@@ -0,0 +1,23 @@
+package dsa.dynamicprogramming;
+
+public class CountStairPaths {
+    public static void main(String[] args) {
+        int n = 10;
+        int ans = countPaths(n, new int[n + 1]);
+        System.out.println(ans);
+    }
+
+    private static int countPaths(int n, int[] qb) {
+        if (n == 0) return 1; // chalo hi mat
+        if (n < 0) return 0;
+
+        if (qb[n] != 0) return qb[n];
+        int oneJump = countPaths(n - 1, qb);
+        int twoJump = countPaths(n - 2, qb);
+        int threeJump = countPaths(n - 3, qb);
+
+        int res = oneJump + twoJump + threeJump;
+        qb[n] = res;
+        return res;
+    }
+}
---
+++ src/dsa/dynamicprogramming/GoldMine.java
@@ -0,0 +1,31 @@
+package dsa.dynamicprogramming;
+
+public class GoldMine {
+    public static void main(String[] args) {
+        int m = 10;
+        int n = 10;
+
+        int a[][] = new int[m][n];
+        int dp[][] = new int[m][n];
+
+        for (int j = a[0].length - 1; j >= 0; j--) {
+            for (int i = a.length - 1; i >= 0; i--) {
+                if (j == a[0].length - 1) {
+                    dp[i][j] = a[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = a[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
+                } else if (i == a.length - 1) {
+                    dp[i][j] = a[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
+                } else {
+                    dp[i][j] = a[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i + 1][j + 1], dp[i - 1][j + 1]));
+                }
+            }
+        }
+
+        int max = dp[0][0];
+        for (int i = 1; i < a.length; i++) {
+            max = Math.max(max, dp[i][0]);
+        }
+        System.out.println(max);
+    }
+}
