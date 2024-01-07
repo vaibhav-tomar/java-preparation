ec82e439556002fd67f43426c17571ffd9cb5058
vaibhav-tomar <vaibhavtomar.tmr@gmail.com>
2024-01-07 17:45:51 +0530

    Add more questions

---
+++ src/systemdesign/designpattern/templatedesignpattern/PayToFriend.java
@@ -0,0 +1,23 @@
+package systemdesign.designpattern.templatedesignpattern;
+
+public class PayToFriend extends PaymentFlow {
+    @Override
+    public void validateRequest() {
+        System.out.println("validate for friend");
+    }
+
+    @Override
+    public void calculateFee() {
+        System.out.println("calculate fee for friend");
+    }
+
+    @Override
+    public void debit() {
+        System.out.println("debit for friend");
+    }
+
+    @Override
+    public void credit() {
+        System.out.println("credit for friend");
+    }
+}
---
+++ src/systemdesign/designpattern/templatedesignpattern/PaymentFlow.java
@@ -0,0 +1,18 @@
+package systemdesign.designpattern.templatedesignpattern;
+
+public abstract class PaymentFlow {
+    public abstract void validateRequest();
+
+    public abstract void calculateFee();
+
+    public abstract void debit();
+
+    public abstract void credit();
+
+    public final void sendMoney() {
+        validateRequest();
+        calculateFee();
+        debit();
+        credit();
+    }
+}
